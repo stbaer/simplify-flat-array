790c9d99cfe7a127d01c51644e13f1f7fb43cc6b
Steffen Baer <stbaer.mail@gmail.com>
2015-05-15 14:27:17 +0800

    working

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# simplify-flat-array
+## simplify-flat-array
+
+[simplify-js](https://github.com/mourner/simplify-js) for flat number arrays ([ x1, y1, x2, y2,...]).
+
+### Install
+
+npm i stbaer/simplify-flat-array
+
+### Use
+
+Like simplify-js, except for the points format. See [simplify-js demo/doku](http://mourner.github.io/simplify-js/)
+
+`simplify(points, tolerance, highQuality)`
+
+    - *points* - [Array] - [ x1, y1, x2, y2,...].
+
+    - *tolerance* - [Number] (optional, 1 by default)
+
+    - *highQuality* [Boolean] (optional, false by default) - includes distance-based processing if true
+
+### Test
+
+`npm run test`
+
+The tests from simplify-js are included, only changed the points format
---
+++ index.js
@@ -0,0 +1,158 @@
+(function () {
+    'use strict';
+
+    /**
+     * square distance between 2 points
+     * @param {[number, number]} p1
+     * @param {[number, number]} p2
+     * @returns {number}
+     */
+    function getSqDist(p1, p2) {
+
+        var dx = p1[0] - p2[0],
+            dy = p1[1] - p2[1];
+
+        return dx * dx + dy * dy;
+    }
+
+    /**
+     * square distance from a point to a segment
+     *
+     * @param {[number, number]} p
+     * @param {[number, number]} p1
+     * @param {[number, number]} p2
+     * @returns {number}
+     */
+    function getSqSegDist(p, p1, p2) {
+
+        var x = p1[0],
+            y = p1[1],
+            dx = p2[0] - x,
+            dy = p2[1] - y;
+
+        if (dx !== 0 || dy !== 0) {
+
+            var t = ((p[0] - x) * dx + (p[1] - y) * dy) / (dx * dx + dy * dy);
+
+            if (t > 1) {
+                x = p2[0];
+                y = p2[1];
+
+            } else if (t > 0) {
+                x += dx * t;
+                y += dy * t;
+            }
+        }
+
+        dx = p[0] - x;
+        dy = p[1] - y;
+
+        return dx * dx + dy * dy;
+    }
+
+    /**
+     * basic distance-based simplification
+     *
+     * @param points
+     * @param sqTolerance
+     * @returns {*[]}
+     */
+    function simplifyRadialDist(points, sqTolerance) {
+
+        var prevPoint = [points[0], points[1]],
+            newPoints = prevPoint,
+            i = 2,
+            n = points.length,
+            point;
+
+        for (i; i < n; i += 2) {
+            point = [points[i], points[i + 1]];
+
+            if (getSqDist(point, prevPoint) > sqTolerance) {
+                newPoints.push(point[0], point[1]);
+                prevPoint = point;
+            }
+        }
+
+        if (prevPoint !== point) {
+            newPoints.push(point[0], point[1]);
+        }
+
+        return newPoints;
+    }
+
+    /**
+     *
+     * @param points
+     * @param first
+     * @param last
+     * @param sqTolerance
+     * @param simplified
+     */
+    function simplifyDPStep(points, first, last, sqTolerance, simplified) {
+        var maxSqDist = sqTolerance,
+            sqDist,
+            index;
+
+        for (var i = first + 2; i < last; i+=2) {
+            sqDist = getSqSegDist([points[i], points[i+1]], [points[first], points[first+1]], [points[last], points[last+1]]);
+
+            if (sqDist > maxSqDist) {
+                index = i;
+                maxSqDist = sqDist;
+            }
+        }
+
+        if (maxSqDist > sqTolerance) {
+            if (index - first > 1) simplifyDPStep(points, first, index, sqTolerance, simplified);
+            simplified.push(points[index], points[index+1]);
+            if (last - index > 1) simplifyDPStep(points, index, last, sqTolerance, simplified);
+        }
+    }
+
+    /**
+     * simplification using Ramer-Douglas-Peucker algorithm
+     *
+     * @param points
+     * @param sqTolerance
+     * @returns {*[]}
+     */
+    function simplifyDouglasPeucker(points, sqTolerance) {
+        var last = points.length - 2;
+
+        var simplified = [points[0], points[1]];
+        simplifyDPStep(points, 0, last, sqTolerance, simplified);
+        simplified.push(points[last], points[last+1]);
+
+        return simplified;
+    }
+
+    /**
+     * both algorithms combined for awesome performance
+     *
+     * @param points
+     * @param tolerance
+     * @param highestQuality
+     * @returns {*}
+     */
+    function simplify(points, tolerance, highestQuality) {
+
+        if (points.length <= 4) return points;
+
+        var sqTolerance = tolerance !== undefined ? tolerance * tolerance : 1;
+
+        points = highestQuality ? points : simplifyRadialDist(points, sqTolerance);
+        points = simplifyDouglasPeucker(points, sqTolerance);
+
+        return points;
+    }
+
+    // export as AMD module / Node module / browser or worker variable
+    if (typeof define === 'function' && define.amd) define(function () {
+        return simplify;
+    });
+    else if (typeof module !== 'undefined') module.exports = simplify;
+    else if (typeof self !== 'undefined') self.simplify = simplify;
+    else window.simplify = simplify;
+
+})();
